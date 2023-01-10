70e456ae5796790260745923b1ee2c4349f8caf3
olpapina <opapina@solvd.com>
2023-01-10 20:07:03 +0300

    verifyEnterIntoAccountTest was implemented

--- src/main/java/com/solvd/eshop/HomePage.java
+++ src/main/java/com/solvd/eshop/HomePage.java
@@ -1,56 +1,130 @@
 package com.solvd.eshop;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class HomePage {
     protected WebDriver driver;
+    private WebDriverWait waiter;
 
-    private By cookieButtonBy = By.cssSelector(".styles_reactButton__2olKd");
-    private By accountMenuBy = By.cssSelector(".userToolsText");
-    private By loginButtonBy = By.cssSelector(".userToolsBtn");
-    private By registrationButtonBy = By.xpath("//*[contains(@class,'styles_bottomLinks__349w0')]//*[contains(text(),'Регистрация')]");
-    private By emailFieldBy = By.cssSelector(".style_inputStyle__1dvyw");
-    private By continueButtonBy = By.cssSelector(".styles_registerButton__18sPZ");
-    private By privacyButton = By.xpath(("//*[@class='styles_buttons__3IGzu']//*[contains(text(),'Соглашаюсь')]"));
-    private By successRegistration = By.cssSelector(".styles_successMessageWrapper__2OOj9");
+    @FindBy(css = ".styles_reactButton__2olKd")
+    private WebElement cookieButton;
+    @FindBy(css = ".styles_userToolsToggler__imcSl")
+    private WebElement accountMenu;
+    @FindBy(css = ".userToolsBtn")
+    private WebElement loginButton;
+    @FindBy(xpath = "//*[contains(@class,'styles_bottomLinks__349w0')]//*[contains(text(),'Регистрация')]")
+    private WebElement registrationButton;
+    @FindBy(css = ".style_inputStyle__1dvyw")
+    private WebElement emailField;
+    @FindBy (css = ".styles_registerButton__18sPZ")
+    private WebElement continueButton;
+    @FindBy (xpath = "//*[@class='styles_buttons__3IGzu']//*[contains(text(),'Соглашаюсь')]")
+    private WebElement privacyButton;
+    @FindBy (css = ".styles_successTitle__1IDi2")
+    private WebElement successRegistration;
+    @FindBy (css = "#login-email")
+    private WebElement loginEmail;
+    @FindBy (css = "#login-password")
+    private WebElement loginPassword;
+    @FindBy (css = ".style_actions__2mIsz")
+    private WebElement enterButton;
+    @FindBy (css = ".userToolsSubtitle")
+    private WebElement accountEmail;
+    @FindBy (css = ".styles_userTools__2J7cp.undefined")
+    private WebElement accountMenuAfterLogin;
+    @FindBy (css = ".styles_catalogButton__1K6kI")
+    private WebElement catalogButton;
 
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        this.waiter = new WebDriverWait(this.driver,Duration.ofMillis(1000));
+        PageFactory.initElements(driver, this);
     }
 
     public void clickCookieButton() {
-        driver.findElement(cookieButtonBy).click();
+        if (cookieButton.isDisplayed()) {
+            cookieButton.click();
+        }
     }
 
     public void clickAccountMenu() {
-        driver.findElement(accountMenuBy).click();
+        if (accountMenu.isDisplayed()) {
+            accountMenu.click();
+        }
     }
 
-    public void clickLoginButton() {
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-        driver.findElement(loginButtonBy).click();
-    }
+        public void clickLoginButton() {
+            waiter.until(ExpectedConditions.elementToBeClickable(loginButton));
+            if (loginButton.isDisplayed()) {
+                loginButton.click();
+            }
+        }
 
     public void clickRegistrationButton() {
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-        driver.findElement(registrationButtonBy).click();
+        waiter.until(ExpectedConditions.elementToBeClickable(registrationButton));
+        if (registrationButton.isDisplayed()) {
+            registrationButton.click();
+        }
     }
 
     public void typeEmail(String email) {
-        driver.findElement(emailFieldBy).sendKeys(email);
+        waiter.until(ExpectedConditions.elementToBeClickable(emailField));
+            emailField.sendKeys(email);
     }
 
     public void clickContinueButton() {
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-        driver.findElement(continueButtonBy).click();
+        waiter.until(ExpectedConditions.elementToBeClickable(continueButton));
+        if (continueButton.isDisplayed()) {
+            continueButton.click();
+        }
     }
 
-    public By clickPrivacyButton() {
-        driver.manage().timeouts().implicitlyWait(Duration.ofMillis(500));
-        driver.findElement(privacyButton).click();
+    public WebElement clickPrivacyButton() {
+        waiter.until(ExpectedConditions.elementToBeClickable(privacyButton));
+        if (privacyButton.isDisplayed()) {
+            privacyButton.click();
+        }
+        waiter.until(ExpectedConditions.elementToBeClickable(successRegistration));
         return successRegistration;
+    }
+
+    public void typeLoginEmail(String email) {
+        waiter.until(ExpectedConditions.elementToBeClickable(loginEmail));
+        if (loginEmail.isDisplayed()) {
+            loginEmail.sendKeys(email);
+        }
+    }
+
+    public void typeLoginPassword(String password) {
+        waiter.until(ExpectedConditions.elementToBeClickable(loginPassword));
+        if (loginPassword.isDisplayed()) {
+            loginPassword.sendKeys(password);
+        }
+    }
+
+    public void clickEnterButton() {
+        waiter.until(ExpectedConditions.elementToBeClickable(enterButton));
+        if (enterButton.isDisplayed()) {
+            enterButton.click();
+        }
+    }
+
+    public void clickAccountMenuAfterLogin() {
+        waiter.until(ExpectedConditions.elementToBeClickable(accountMenuAfterLogin));
+        if (accountMenuAfterLogin.isDisplayed()) {
+            accountMenuAfterLogin.click();
+        }
+    }
+
+    public String getAccountInfo() {
+        waiter.until(ExpectedConditions.elementToBeClickable(accountEmail));
+        return accountEmail.getText();
     }
 }
--- src/test/java/com/solvd/eshop/AccountSectionTest.java
+++ src/test/java/com/solvd/eshop/AccountSectionTest.java
@@ -1,22 +1,27 @@
 package com.solvd.eshop;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
+
 public class AccountSectionTest {
 
-    private WebDriver driver;
+    WebDriver driver;
+    ConfigFileReader configFileReader;
 
     @BeforeTest
     public void setupBeforeTest() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("https://www.21vek.by");
+        driver.get(ConfigFileReader.getData("url"));
         HomePage homePage = new HomePage(driver);
         homePage.clickCookieButton();
     }
@@ -27,12 +32,28 @@ public class AccountSectionTest {
         homePage.clickAccountMenu();
         homePage.clickLoginButton();
         homePage.clickRegistrationButton();
-        homePage.typeEmail("freppeumeproiffei-9159@yopmail.com");
+        int num = (int)(Math.random() *9999);
+        homePage.typeEmail("freppeumeproiffei-" + Integer.toString(num) + "@yopmail.com");
         homePage.clickContinueButton();
-        By newSuccessRegistration = homePage.clickPrivacyButton();
-        Assert.assertNotNull(newSuccessRegistration, "Registration isn't completed");
+        WebElement newSuccessRegistration = homePage.clickPrivacyButton();
+        String successMessage = newSuccessRegistration.getText();
+        Assert.assertEquals(successMessage, "Вы зарегистрированы", "Registration success message wasn't displayed");
     }
 
+    @Test(testName = "verify enter into account")
+    public void verifyEnterIntoAccountTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.clickAccountMenu();
+        homePage.clickLoginButton();
+        homePage.typeLoginEmail("freppeumeproiffei-9147@yopmail.com");
+        homePage.typeLoginPassword("7a5a23b6");
+        homePage.clickEnterButton();
+        homePage.clickAccountMenuAfterLogin();
+        String accountEmail = homePage.getAccountInfo();
+        Assert.assertEquals(accountEmail, "freppeumeproiffei-9147@yopmail.com","Login is failed");
+    }
+
+    @AfterTest (alwaysRun = true)
     public void setupAfterTest() {
         driver.close();
     }
